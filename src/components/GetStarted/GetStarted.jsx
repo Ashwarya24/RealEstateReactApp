d34524306c9225e495bfa6ef6b9536d14cbc589c
Ashwarya Agarwal <77434287+Ashwarya24@users.noreply.github.com>
2024-05-27 08:50:10 +0530

    Add files via upload

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './GetStarted.css'
+const GetStarted = () => {
+  return (
+   <section id="get-started" className='g-wrapper'>
+    <div className="paddings innerWidth g-container">
+        <div className="flexColCenter inner-container">
+            <span className='primaryText'>Get Started with Homyz</span>
+            <span className='secondaryText'>Subscribe and find super attractive home locations at affordable prices.<br />
+            Find Your Home . Come Abode!</span>
+            <button className='button'>
+                <a href="mailto:ashwarya240@gmail.com">Get Started</a>
+            </button>
+        </div>
+    </div>
+   </section>
+  )
+}
+
+export default GetStarted
