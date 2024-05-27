af9a1f6766b04e216f2f601b40b7a420b0773e32
Ashwarya Agarwal <77434287+Ashwarya24@users.noreply.github.com>
2024-05-27 08:45:05 +0530

    Add files via upload

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+      "Looking to buy your dream home? Our competitive interest rates make it easier than ever! Whether you're a first-time homebuyer or looking to refinance, our tailored loan options are designed to meet your needs.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "Searching for your dream home? Our real estate app offers stability in a volatile market. Stay ahead with reliable pricing data and access the best deals available. Whether you're a seasoned buyer or new to the market, our app ensures you find the most competitive prices.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "Dreaming of your perfect home? Discover stable, accurate prices with our cutting-edge real estate app! Say goodbye to unpredictable market shifts and hello to the best deals available. Whether you're a first-time buyer or an experienced investor, our app ensures you get the most reliable prices.",
+  },
+];
+export default data;
